257dfbf4448c73ae1eadcb57f2c8ea264cc7e2e1
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-07 22:55:58 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from os import listdir
+from os.path import isfile, join
+import glob
+
 app = Flask(__name__)
 
 @app.route("/")
