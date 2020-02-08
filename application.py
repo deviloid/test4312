0995420e3a20cdacda33e6352afef36ac8936325
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-07 22:50:10 -0700

    reverting changes

--- application.py
+++ application.py
@@ -1,18 +1,6 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-# from app_controller import user_controller
-from os import listdir
-from os.path import isfile, join
-import glob
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-
-    return render_template("hostingstart.html")
-    # return "Meri pyari Kachuyi! 😘😘😘"
-
-if __name__ == '__main__':
-    app.run()
+    return "Hello World!"
