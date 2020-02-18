76b68fcf125694ea6fe4a7ff8fb8cc96c58660ea
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-18 16:18:29 -0700

    Test

--- application.py
+++ application.py
@@ -3,17 +3,6 @@ from flask_sqlaalchemy import flask_sqlaalchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-db = SQLAlchemy(app)
-
-class Todo(db.model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r' % self.id
 
 @app.route('/')
 def index():
