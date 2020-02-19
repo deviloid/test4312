99cd92dfc6867529448aa5a1a41d3713842c4c2d
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-18 19:26:37 -0700

    Update tapp1.py

--- tapp1.py
+++ tapp1.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
