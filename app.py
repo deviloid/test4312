33c438358d8f7d26cf2eed99a5848e8462f04c3c
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-28 01:13:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 from datetime import datetime
 
 app = Flask(__name__)
