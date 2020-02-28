ccca06cf8c49f1b498c4bb1e52fb3f4af88094de
Sayantan Mridha <sayantan.mridha@hotmail.com>
2020-02-28 02:05:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,40 +19,42 @@ class Model(mysql.Model):
 def index():
     if request.method=='POST':
         task_content = request.form['content']
-        new_task = Model(content = task_content)
+        cur = mysql.connection.cursor()
+        new_task = cur.execute("INSERT INTO Model(content1) VALUES (%s)", (content1))
         try:
             mysql.connection.add(new_task)
             mysql.connection.commit()
+            mysql.close()
             return redirect('/')
         except:
             return 'There was an issue adding your input.'
 
     else:
-        tasks = Model.query.order_by(Model.date_created).all()
+        tasks = cur.execute("SELECT * FROM Model ORDER BY date_created")
         return render_template('index.html', tasks = tasks)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Model.query.get_or_404(id)
-    try:
-        mysql.connection.delete(task_to_delete)
-        mysql.connection.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task.'
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Model.query.get_or_404(id)
+#     try:
+#         mysql.connection.delete(task_to_delete)
+#         mysql.connection.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task.'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Model.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-        try:
-            mysql.connection.commit()
-            return redirect('/')
-        except:
-            'There was an issue updating your task.'
-    else:
-        return render_template('update.html', task = task)
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     task = Model.query.get_or_404(id)
+#     if request.method == 'POST':
+#         task.content = request.form['content']
+#         try:
+#             mysql.connection.commit()
+#             return redirect('/')
+#         except:
+#             'There was an issue updating your task.'
+#     else:
+#         return render_template('update.html', task = task)
 
 
 if __name__ == "__main__":
